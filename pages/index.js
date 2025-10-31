a624ddb30a95e6ac137569c99f5c7791a2243465
n.moreira <nicholascm@gmail.com>
2025-10-31 15:56:26 -0300

    alteração de mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <header><h1>Nínguem é tão grande que não possa aprender.</h1> <h6>Nem tão pequeno, que não possa ensinar!</h6></header>;
+    return <header><h1>Nínguem é tão grande que não possa aprender.🧠</h1> <h6>Nem tão pequeno, que não possa ensinar!🧑‍🏫</h6></header>;
 }
 
 export default Home;
